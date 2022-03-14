c40042d31773cc34af7e3ccb30650f2004c747db
zayong <zayong@feelway.com>
2022-03-14 17:56:09 +0900

    update

---
+++ app.js
@@ -0,0 +1,10 @@
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+function onLoginBtnClick() {
+    if(username === ""){
+
+    }
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
